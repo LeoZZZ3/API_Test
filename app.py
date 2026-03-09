dd382d8b5df2012a77ead1634c64277ad941bbf7
LeoZZZ3 <zagoleo2002@gmail.com>
2026-03-09 09:58:18 +0100

    tout marche

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
 import os
 import mysql.connector
-
+from flask_cors import CORS
 from flask import Flask, jsonify, request
 from dotenv import load_dotenv
+
+app = Flask(__name__)
+CORS(app)
 
 load_dotenv()
 
